7eb37cc322e9bf49f6a2b21fdf559553694cfcc6
abdul1ah33 <study.abdullah2005@gmail.com>
2024-10-06 20:41:55 +0000

    12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,8 +2,8 @@ export default function createReportObject(employeesList) {
   return {
     allEmployees: employeesList,
     // eslint-disable-next-line no-shadow
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length;
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
     },
   };
 }
