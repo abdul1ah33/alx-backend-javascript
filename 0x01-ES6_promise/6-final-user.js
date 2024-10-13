e14b7e39f60f851b63c87aadad86cb99e9880683
abdul1ah33 <study.abdullah2005@gmail.com>
2024-10-13 21:43:18 +0000

    dsf

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,10 +5,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   // Call the two functions with the necessary arguments
   return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
+    uploadPhoto(fileName),
   ])
-  .then(results => results.map(result => ({
+    .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : String(result.reason)
-  })));
+      value: result.status === 'fulfilled' ? result.value : String(result.reason),
+    })));
 }
